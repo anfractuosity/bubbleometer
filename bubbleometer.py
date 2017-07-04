e297509e1503bbdf4142241a686034f4f1ba7947
anfractuosity <anfractuosity@anfractuosity.com>
2017-07-04 14:24:26 +0100

    remove old code

--- bubbleometer.py
+++ bubbleometer.py
@@ -17,19 +17,6 @@ wf = wave.open(sys.argv[1], 'rb')
 print("chans ",wf.getnchannels())
 # instantiate PyAudio (1)
 p = pyaudio.PyAudio()
-
-"""
-# open stream (2)
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
-
-# read data
-data = wf.readframes(CHUNK)
-"""
-
-
 bubbles = []
 
 
@@ -51,9 +38,6 @@ def callback(in_data, frame_count, time_info, status):
         print("  ")                                                              
     return (data, pyaudio.paContinue)
 
-
-
-
 # open stream using callback (3)
 stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                 channels=wf.getnchannels(),
@@ -64,8 +48,6 @@ stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
 # start the stream (4)
 stream.start_stream()
 
-
-
 # wait for stream to finish (5)
 while stream.is_active():
     time.sleep(0.1)
@@ -74,46 +56,6 @@ while stream.is_active():
 stream.stop_stream()
 stream.close()
 wf.close()
-
 # close PyAudio (7)
 p.terminate()
-
 print(bubbles)
-""""
-quit()
-
-
-
-
-
-
-# play stream (3)
-while len(data) > 0:
-
-    s = np.frombuffer(data,dtype="<i2")
-    fft = np.fft.fft(s)
-   
-    mags = []
-    c = 0
-    for v in fft:
-        if c > 70 and c < 100:
-            mag = np.sqrt((v.real**2)+(v.imag**2))
-            mags.append(mag)
-        c += 1
-    
-    if (np.sort(mags)[::-1][0]) > 9000:
-        print("bubble")
-    else:
-        print("  ")
-    stream.write(data)
-    data = wf.readframes(CHUNK)
-
-# stop stream (4)
-stream.stop_stream()
-stream.close()
-
-# close PyAudio (5)
-p.terminate()
-
-"""
-
