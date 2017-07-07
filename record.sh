3a6201e02318c7978e803b180d144c6426f51772
anfractuosity <anfractuosity@anfractuosity.com>
2017-07-07 22:55:30 +0100

    recording

--- record.sh
+++ record.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# log start time
 date > start
 
-SEC=`expr 60 \* 60 \* 24`
+# record in hourly chunks
+SEC=`expr 60 \* 60`
 echo $SEC
 
+# start recording
 sox -d bubbles_.wav channels 1 rate 48k trim 0 $SEC : newfile : restart
